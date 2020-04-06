230e99f61c565c8586394bbe9f09ad3077fce99a
Tai Beo <tai.nguyentrung1996@gmail.com>
2020-04-06 10:47:26 +0700

    update code giathuochapu

--- Python/Crawl_Data/TO_EXCEL/Crawling_Medicine_To_Excel/giathuochapu/giathuochapu_to_excel.py
+++ Python/Crawl_Data/TO_EXCEL/Crawling_Medicine_To_Excel/giathuochapu/giathuochapu_to_excel.py
@@ -11,8 +11,11 @@ from xlutils.copy import copy as xl_copy
 from openpyxl.styles import Font
 
 from timeit import default_timer as timer
-from datetime import timedelta
+from datetime import timedelta, date
 import json
+
+# pip3 install fuzzywuzzy
+# pip3 install python-Levenshtein
 
 class ToExcel:
   def write_row_to_excel(sheet, text_data, row, column):
@@ -21,7 +24,7 @@ class ToExcel:
   def create_headers_to_excel(sheet_name, excel_name_file):
     book = xlwt.Workbook(encoding="utf-8", style_compression=0)
 
-    headers = ("Product Id", "Product Name", "Product Price", 'Img URL', 'Packing')
+    headers = ("Product Id", "Product Name", "Product Price", 'Product URL', 'Img URL', 'Packing')
     bsheet = book.add_sheet(sheet_name, cell_overwrite_ok=True)
     style = xlwt.easyxf('align: vert centre, horiz centre; font: bold on')
 
@@ -31,7 +34,7 @@ class ToExcel:
 
 class GiaThuocHapu:
   def requests_get(url):
-    req = requests.get(url, stream=True, timeout=15)
+    req = requests.get(url, stream=True, timeout=20)
     soup = BeautifulSoup(req.text, 'html.parser')
     return req
 
@@ -45,38 +48,38 @@ class GiaThuocHapu:
 
       img_url = ''
       packing = ''
-      detail_product = {'img_url': img_url, 'packing': packing}
+      product_url = ''
+      detail_product = {'product_url': product_url, 'img_url': img_url, 'packing': packing}
 
       if find_img is not None:
         img_url = find_img.get('data-original')
         detail_product['img_url'] = img_url
-        print("item....... {} ------".format(img_url))
 
-      # go to detail url vd: https://giathuochapu.com/san-pham/actiso-ong-hau-giang/
       try:
-        detail_url = thumb_soup.find('a', class_='more-product')
-        req_detail_url = requests.get(detail_url.get('href'), timeout=10, stream=True)
+        # go to detail url vd: https://giathuochapu.com/san-pham/actiso-ong-hau-giang/
+        detail_url = thumb_soup.find('a', re.compile(r'button more-product'))
+        req_detail_url = requests.get(detail_url.get('href'), timeout=20, stream=True)
         detail_soup = BeautifulSoup(req_detail_url.text, 'html.parser')
 
         if detail_url is None and find_img is None:
           thumbs_data.append(detail_product)
-          continue
         else:
+          product_url = detail_url.get('href')
+          detail_product['product_url'] = product_url
+
           find_table = detail_soup.find('table')
           # print(">>>>>> find table {}", find_table)
 
           if find_table is None:
             thumbs_data.append(detail_product)
-            continue
 
           if find_table is not None:
             find_tr = find_table.find('tr')
 
             if find_tr is None:
               thumbs_data.append(detail_product)
-              continue
             else:
-              find_td = find_tr.find('td', text=re.compile('Quy cách:'))
+              find_td = find_tr.find('td', text=re.compile(r'Quy cách:'))
 
               if find_td is not None:
                 find_next_td = find_tr.td.find_next_sibling('td')
@@ -88,99 +91,54 @@ class GiaThuocHapu:
                   thumbs_data.append(detail_product)
                 else:
                   thumbs_data.append(detail_product)
-                  continue
               else:
                 thumbs_data.append(detail_product)
-                continue
-      except (requests.exceptions.RequestException, ValueError) as e:
-        print('>>> Error ValueError: {}'.format(e))
-        thumbs_data.append(detail_product)
-        continue
+      except requests.exceptions.Timeout as e:
+        print('>>> Error Exception: {}'.format(e))
+        detail_url = thumb_soup.find('a', re.compile(r'button more-product'))
+        req_detail_url = requests.get(detail_url.get('href'), timeout=20, stream=True)
+        detail_soup = BeautifulSoup(req_detail_url.text, 'html.parser')
+
+        if detail_url is None and find_img is None:
+          thumbs_data.append(detail_product)
+        else:
+          product_url = detail_url.get('href')
+          detail_product['product_url'] = product_url
+
+          find_table = detail_soup.find('table')
+          # print(">>>>>> find table {}", find_table)
+
+          if find_table is None:
+            thumbs_data.append(detail_product)
+
+          if find_table is not None:
+            find_tr = find_table.find('tr')
+
+            if find_tr is None:
+              thumbs_data.append(detail_product)
+            else:
+              find_td = find_tr.find('td', text=re.compile(r'Quy cách:'))
+
+              if find_td is not None:
+                find_next_td = find_tr.td.find_next_sibling('td')
+
+                if find_next_td is not None:
+                  packing = find_next_td.get_text(strip=True) # quy cach dong goi
+                  detail_product['packing'] = packing
+                  # print(">>>>>>> Go to packing?", packing)
+                  thumbs_data.append(detail_product)
+                else:
+                  thumbs_data.append(detail_product)
+              else:
+                thumbs_data.append(detail_product)
       except requests.exceptions.ConnectionError as e:
-        print(">>>> Error Connection Error: {}".format(e))
-        # Try to Request Detail URL again
-        detail_url = thumb_soup.find('a', class_='more-product')
-        req_detail_url = requests.get(detail_url.get('href'), timeout=10, stream=True)
-        detail_soup = BeautifulSoup(req_detail_url.text, 'html.parser')
-
-        if detail_url is None and find_img is None:
-          thumbs_data.append(detail_product)
-          continue
-        else:
-          find_table = detail_soup.find('table')
-          # print(">>>>>> find table {}", find_table)
-
-          if find_table is None:
-            thumbs_data.append(detail_product)
-            continue
-
-          if find_table is not None:
-            find_tr = find_table.find('tr')
-
-            if find_tr is None:
-              thumbs_data.append(detail_product)
-              continue
-            else:
-              find_td = find_tr.find('td', text=re.compile('Quy cách:'))
-
-              if find_td is not None:
-                find_next_td = find_tr.td.find_next_sibling('td')
-
-                if find_next_td is not None:
-                  packing = find_next_td.get_text(strip=True) # quy cach dong goi
-                  detail_product['packing'] = packing
-                  # print(">>>>>>> Go to packing?", packing)
-                  thumbs_data.append(detail_product)
-                else:
-                  thumbs_data.append(detail_product)
-                  continue
-              else:
-                thumbs_data.append(detail_product)
-                continue
-      except requests.exceptions.Timeout:
-        # Maybe set up for a retry, or continue in a retry loop
-        detail_url = thumb_soup.find('a', class_='more-product')
-        req_detail_url = requests.get(detail_url.get('href'), timeout=10, stream=True)
-        detail_soup = BeautifulSoup(req_detail_url.text, 'html.parser')
-
-        if detail_url is None and find_img is None:
-          thumbs_data.append(detail_product)
-          continue
-        else:
-          find_table = detail_soup.find('table')
-          # print(">>>>>> find table {}", find_table)
-
-          if find_table is None:
-            thumbs_data.append(detail_product)
-            continue
-
-          if find_table is not None:
-            find_tr = find_table.find('tr')
-
-            if find_tr is None:
-              thumbs_data.append(detail_product)
-              continue
-            else:
-              find_td = find_tr.find('td', text=re.compile('Quy cách:'))
-
-              if find_td is not None:
-                find_next_td = find_tr.td.find_next_sibling('td')
-
-                if find_next_td is not None:
-                  packing = find_next_td.get_text(strip=True) # quy cach dong goi
-                  detail_product['packing'] = packing
-                  # print(">>>>>>> Go to packing?", packing)
-                  thumbs_data.append(detail_product)
-                else:
-                  thumbs_data.append(detail_product)
-                  continue
-              else:
-                thumbs_data.append(detail_product)
-                continue
+        thumbs_data.append(detail_product)
       except requests.exceptions.RequestException as e:
         # catastrophic error. bail.
         raise SystemExit(e)
+        thumbs_data.append(detail_product)
         continue
+      print("stt....... {} ------- detail {}".format(item, detail_product))
     return thumbs_data
 
   def get_results_giathuochapu(req):
@@ -193,17 +151,18 @@ class GiaThuocHapu:
     script_replace_2 = script_replace_1.replace(',"ajaxUrl":"https://giathuochapu.com/wp-admin/admin-ajax.php"};\n/* ]]> */\n', '')
 
     thumbs_data = GiaThuocHapu.get_thumbs_data(script_replace_2)
+    data_remove_thumb = re.sub(r'"thumb":"(.*?)",', "", script_replace_2)
+    data_remove_expire_date = re.sub(r'"expiration_date":"(.*?)",', "", data_remove_thumb)#.replace(r'<i class="fa fa-line-chart"></i>', '').replace(r'<i class="fa fa-long-arrow-up"></i>', '').replace('<i class="fa fa-long-arrow-down red"></i>', '')
+    real_data = re.sub(r'<i (.*?)></i>', "", data_remove_expire_date)
 
-    remove_thumb = re.sub(r'"thumb":"(.*?)",', "", script_replace_2)
-    remove_expire_date = re.sub(r'"expiration_date":"(.*?)",', "", remove_thumb)
-
-    remove_tag_html = re.sub('<.*?>', '', remove_expire_date)
+    data = json.loads(real_data)
+    # print(">>>>>>>>>>>>> data: {}", len(data))
+    # print(">>>>>>>>>>>>> data: {}", len(thumbs_data))
 
     results = []
-    for i in range(len(thumbs_data)):
-      data = json.loads(remove_tag_html)[i]
-      data.update(thumbs_data[i])
-      results.append(data)
+    for i in range(len(data)):
+      data[i].update(thumbs_data[i])
+      results.append(data[i])
 
     return results
 
@@ -212,6 +171,7 @@ class GiaThuocHapu:
     count_item = 0
 
     print(">>>> Total: {}".format(len(results)))
+
     for item in range(len(results)):
       data = results[item]
 
@@ -219,6 +179,7 @@ class GiaThuocHapu:
       product_name = data['name']
       product_price = float(data['item_vip_price']) * 1000
       product_type = data['loai_sp']
+      product_url = data['product_url']
       product_image = data['img_url']
       product_packing = data['packing']
 
@@ -229,10 +190,11 @@ class GiaThuocHapu:
       ToExcel.write_row_to_excel(sheet, product_id, line, 0)
       ToExcel.write_row_to_excel(sheet, product_name, line, 1)
       ToExcel.write_row_to_excel(sheet, product_price, line, 2)
-      ToExcel.write_row_to_excel(sheet, product_image, line, 3)
-      ToExcel.write_row_to_excel(sheet, product_packing, line, 4)
+      ToExcel.write_row_to_excel(sheet, product_url, line, 3)
+      ToExcel.write_row_to_excel(sheet, product_image, line, 4)
+      ToExcel.write_row_to_excel(sheet, product_packing, line, 5)
 
-      print(f">>>>>>> id: {product_id} ----- Name: {product_name} -------- Price: {product_price}")
+      # print(f">>>>>>> id: {product_id} ----- Name: {product_name} -------- Price: {product_price}")
 
       count_item = count_item + 1
       wb_copy.save(excel_name_file)
@@ -254,7 +216,7 @@ if __name__ == "__main__":
   req = GiaThuocHapu.requests_get(url_order)
 
   start = timer()
-  print("Start crawling at: {}".format(timedelta(seconds=start)))
+  print("Start crawling 00:00:00")
 
   results = GiaThuocHapu.get_results_giathuochapu(req)
   GiaThuocHapu.save_data_to_excel(results)
